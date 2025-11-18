4a7a5e6afc513744a3190ef2c6248e96a5a87542
stash <pomichterstash@gmail.com>
2025-11-18 00:43:29 +0000

    Fixed ctransformers cuda 12 bug during docker up
    Former-commit-id: 16267c6d729109b1984c1690603ae976d0d693e5 [formerly 26022da5f9868789a4ead8e7b6a0dde481e9262d] [formerly b642b263e051bc36d91e580b3636785d2096414b [formerly c67863fae4b6daece312ff16666426085a5354df]]
    Former-commit-id: c68332fe1483428a6eb10dd6106064af2db7e5c7
    Former-commit-id: e1994b8d1a0ca50bd9978fc96577001ac5e42324

--- tests/test_agent_ctransformers_gguf.py
+++ tests/test_agent_ctransformers_gguf.py
@@ -15,39 +15,28 @@
 import tests.test_header
 
 from dimos.agents.agent_ctransformers_gguf import CTransformersGGUFAgent
-from dimos.stream.data_provider import QueryDataProvider
 
-# Initialize query stream
-query_provider = QueryDataProvider()
+system_query = "You are a robot with the following functions. Move(), Reverse(), Left(), Right(), Stop(). Given the following user comands return the correct function."
 
 # Initialize agent
-agent_observable = CTransformersGGUFAgent(
+agent = CTransformersGGUFAgent(
     dev_name="GGUF-Agent",
     model_name="TheBloke/Llama-2-7B-GGUF",
     model_file="llama-2-7b.Q4_K_M.gguf",
     model_type="llama",
+    system_query=system_query,
     gpu_layers=50,
     max_input_tokens_per_request=250,
     max_output_tokens_per_request=10,
-    input_query_stream=query_provider.data_stream
-).get_response_observable()
-
-# Subscribe to the observable query stream
-agent_observable.subscribe(
-    on_next=lambda response: print(f"Response: {response}"),
-    on_error=lambda error: print(f"Error: {error}"),
-    on_completed=lambda: print("Completed.")
 )
 
-# Start the query stream.
-# Queries will be pushed every 1 second, in a count from 1 to 5000.
-query_provider.start_query_stream(
-    query_template=
-    "{query}; Denote the number at the beginning of this query before the semicolon as the 'reference number'. Provide the reference number, without any other text in your response.",
-    frequency=1,
-    start_count=1,
-    end_count=5000,
-    step=1)
+test_query = "User: Travel forward 10 meters"
+
+agent.run_observable_query(test_query).subscribe(
+    on_next=lambda response: print(f"One-off query response: {response}"),
+    on_error=lambda error: print(f"Error: {error}"),
+    on_completed=lambda: print("Query completed")
+)
 
 try:
     input("Press ESC to exit...")
