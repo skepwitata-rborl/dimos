81f3bb42ee342a8773596bef343630564a48b79e
Lukas Paukstys <lukas.a.paukstys@gmail.com>
2025-11-23 08:10:22 +0000

    Consolodates REST skill and adds manual test.
    Former-commit-id: 85bab32d7179d08f065eb1d929f07ac8eed1fd9b [formerly fa82a7a6cb96dd73feeca09e73fce2cdcda958f5] [formerly 14b5e16c9116ad5779ed8b338af2c8cc8440abd0 [formerly 87c0d13cdc5e34849a9fefc01635773b9c46831e]]
    Former-commit-id: 54b2ca1a562688d6e5e064d42909f853c2346bbd
    Former-commit-id: 452001f83b564fc4c3c7eeab4b89bbc11bf97a5b

---
+++ tests/test_skills_rest.py
@@ -0,0 +1,95 @@
+# Copyright 2025 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tests.test_header
+import os
+
+import time
+from dotenv import load_dotenv
+from dimos.agents.claude_agent import ClaudeAgent
+from dimos.robot.unitree.unitree_go2 import UnitreeGo2
+from dimos.robot.unitree.unitree_ros_control import UnitreeROSControl
+from dimos.robot.unitree.unitree_skills import MyUnitreeSkills
+from dimos.web.robot_web_interface import RobotWebInterface
+from dimos.skills.observe_stream import ObserveStream
+from dimos.skills.kill_skill import KillSkill
+from dimos.skills.navigation import Navigate, BuildSemanticMap
+from dimos.skills.visual_navigation_skills import NavigateToObject, FollowHuman
+from dimos.skills.rest.rest import GenericRestSkill
+import reactivex as rx
+import reactivex.operators as ops
+
+# Load API key from environment
+load_dotenv()
+
+skills = MyUnitreeSkills()
+
+robot = UnitreeGo2(ip=os.getenv('ROBOT_IP'),
+                    ros_control=UnitreeROSControl(),
+                    skills=skills,
+                    mock_connection=True)
+
+# Create a subject for agent responses
+agent_response_subject = rx.subject.Subject()
+agent_response_stream = agent_response_subject.pipe(ops.share())
+
+streams = {"unitree_video": robot.get_ros_video_stream()}
+text_streams = {
+    "agent_responses": agent_response_stream,
+}
+
+web_interface = RobotWebInterface(port=5555, text_streams=text_streams, **streams)
+
+# Create a ClaudeAgent instance
+agent = ClaudeAgent(
+    dev_name="test_agent",
+    input_query_stream=web_interface.query_stream,
+    skills=skills,
+    system_query="""You are an agent controlling a virtual robot. When given a query, respond by using the appropriate tool calls if needed to execute commands on the robot.
+
+IMPORTANT INSTRUCTIONS:
+1. Each tool call must include the exact function name and appropriate parameters
+2. If a function needs parameters like 'distance' or 'angle', be sure to include them
+3. If you're unsure which tool to use, choose the most appropriate one based on the user's query
+4. Parse the user's instructions carefully to determine correct parameter values
+
+Example: If the user asks to move forward 1 meter, call the Move function with distance=1""",
+    model_name="claude-3-7-sonnet-latest",
+    thinking_budget_tokens=2000
+)
+
+skills.add(GenericRestSkill)
+skills.add(ObserveStream)
+skills.add(KillSkill)
+skills.add(Navigate)
+skills.add(BuildSemanticMap)
+skills.add(NavigateToObject)
+skills.add(FollowHuman)
+
+skills.create_instance("ObserveStream", robot=robot, agent=agent)
+skills.create_instance("KillSkill", robot=robot, skill_library=skills)
+skills.create_instance("Navigate", robot=robot)
+skills.create_instance("BuildSemanticMap", robot=robot)
+skills.create_instance("NavigateToObject", robot=robot)
+skills.create_instance("FollowHuman", robot=robot)
+
+# Subscribe to agent responses and send them to the subject
+agent.get_response_observable().subscribe(
+    lambda x: agent_response_subject.on_next(x)
+)
+
+print("ObserveStream and Kill skills registered and ready for use")
+print("Created memory.txt file")
+
+web_interface.run()
