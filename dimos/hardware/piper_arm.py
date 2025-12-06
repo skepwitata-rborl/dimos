5ef9b99970a29b288b6ce9e49bd7fb4ac66f4c43
mustafab0 <39084056+mustafab0@users.noreply.github.com>
2025-12-06 22:30:55 +0000

    CI code cleanup
    Former-commit-id: 6c7b5f5677cbd0f13e278d15162ea58eb7f432f3 [formerly 4ca7cbd6ca13bee33837c78ac7e21bf0a3830110]
    Former-commit-id: 034e20cc7dfe0c80ec8fa09149be9c8c56e947e8

--- dimos/hardware/piper_arm.py
+++ dimos/hardware/piper_arm.py
@@ -37,10 +37,13 @@ class PiperArm:
 
     def init_can(self):
         result = subprocess.run(
-            ["bash", "dimos/dimos/hardware/can_activate.sh"],       # pass the script path directly if it has a shebang and execute perms
-            stdout=subprocess.PIPE,             # capture stdout
-            stderr=subprocess.PIPE,             # capture stderr
-            text=True                           # return strings instead of bytes
+            [
+                "bash",
+                "dimos/dimos/hardware/can_activate.sh",
+            ],  # pass the script path directly if it has a shebang and execute perms
+            stdout=subprocess.PIPE,  # capture stdout
+            stderr=subprocess.PIPE,  # capture stderr
+            text=True,  # return strings instead of bytes
         )
 
     def enable(self):
@@ -50,24 +53,21 @@ class PiperArm:
         print(f"[PiperArm] Enabled")
         self.arm.MotionCtrl_2(0x01, 0x01, 80, 0x00)
 
-
-
     def gotoZero(self):
-        factor = 57295.7795 #1000*180/3.1415926
-        position = [0,0,0,0,0,0,0]
-        
-        joint_0 = round(position[0]*factor)
-        joint_1 = round(position[1]*factor)
-        joint_2 = round(position[2]*factor)
-        joint_3 = round(position[3]*factor)
-        joint_4 = round(position[4]*factor)
-        joint_5 = round(position[5]*factor)
-        joint_6 = round(position[6]*1000*1000)
+        factor = 57295.7795  # 1000*180/3.1415926
+        position = [0, 0, 0, 0, 0, 0, 0]
+
+        joint_0 = round(position[0] * factor)
+        joint_1 = round(position[1] * factor)
+        joint_2 = round(position[2] * factor)
+        joint_3 = round(position[3] * factor)
+        joint_4 = round(position[4] * factor)
+        joint_5 = round(position[5] * factor)
+        joint_6 = round(position[6] * 1000 * 1000)
         self.arm.ModeCtrl(0x01, 0x01, 30, 0x00)
         self.arm.JointCtrl(joint_0, joint_1, joint_2, joint_3, joint_4, joint_5)
         self.arm.GripperCtrl(abs(joint_6), 1000, 0x01, 0)
         pass
-
 
     def softStop(self):
         self.gotoZero()
@@ -76,13 +76,14 @@ class PiperArm:
         self.arm.MotionCtrl_1(0x01, 0, 0)
         time.sleep(5)
 
-
     def cmd_EE_pose(self, x, y, z, r, p, y_):
         """Command end-effector to target pose in space (position + Euler angles)"""
         factor = 1000
         pose = [x * factor, y * factor, z * factor, r * factor, p * factor, y_ * factor]
         self.arm.MotionCtrl_2(0x01, 0x00, 100, 0x00)
-        self.arm.EndPoseCtrl(int(pose[0]), int(pose[1]), int(pose[2]), int(pose[3]), int(pose[4]), int(pose[5]))
+        self.arm.EndPoseCtrl(
+            int(pose[0]), int(pose[1]), int(pose[2]), int(pose[3]), int(pose[4]), int(pose[5])
+        )
         print(f"[PiperArm] Moving to pose: {pose}")
 
     def get_EE_pose(self):
@@ -105,8 +106,8 @@ class PiperArm:
 
     def disable(self):
         self.softStop()
-        
-        while(self.arm.DisablePiper()):
+
+        while self.arm.DisablePiper():
             pass
             time.sleep(0.01)
         self.arm.DisconnectPort()
@@ -125,7 +126,6 @@ if __name__ == "__main__":
         time.sleep(1)
 
         user_input = input("Press Enter to repeat, or type 'q' to quit: ")
-        if user_input.strip().lower() == 'q':
+        if user_input.strip().lower() == "q":
             arm.disable()
             break
-    
